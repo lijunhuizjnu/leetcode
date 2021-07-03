eff7b05edfe248fe6423addc9a5498e694e6a2bf
lijunhuizjnu <44026755+lijunhuizjnu@users.noreply.github.com>
2021-07-03 14:48:09 +0800

    Create 451.cpp

---
+++ C++/451.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        map<char, int> mp;
+        int length = s.length();
+        for (auto &ch : s) {
+            mp[ch]++;
+        }
+        vector<pair<char, int>> vec;
+        for (auto &it : mp) {
+            vec.emplace_back(it);//the affect of useremplace_back is same to push_back. but it dose not create objects and direct insertion. So it is faster.
+        }
+        sort(vec.begin(), vec.end(), [](const pair<char, int> &a, const pair<char, int> &b) {
+            return a.second > b.second;
+        });
+        string ret;
+        for (auto &[ch, num] : vec) {
+            for (int i = 0; i < num; i++) {
+                ret.push_back(ch);
+            }
+        }
+        return ret;
+    }
+};
