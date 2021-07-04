08603db85abad8deafa39bc46c25fe7f7fca646b
lijunhuizjnu <44026755+lijunhuizjnu@users.noreply.github.com>
2021-07-04 12:59:54 +0800

    Update and rename 65.py to 65.cpp

---
+++ C++/65.cpp
@@ -0,0 +1,1 @@
+# write in python3
--- C++/65.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def findErrorNums(self, nums: List[int]) -> List[int]:
-        n=len(nums)
-        
-        a=sum(nums)-sum(set(nums))
-        b=sum(set(list(range(1,n+1))))-sum(set(nums))
-        return [a,b]
