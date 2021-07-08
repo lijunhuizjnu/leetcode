c179af5e8953b97f8e9497e2db993f4b1e550470
lijunhuizjnu <44026755+lijunhuizjnu@users.noreply.github.com>
2021-07-08 11:10:02 +0800

    Create 160.cpp

---
+++ C++/160.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(!headA  || !headB) return NULL;
+        ListNode *pA = headA, *pB = headB;
+        // 在这里第一轮体现在pA和pB第一次到达尾部会移向另一链表的表头, 而第二轮体现在如果pA或pB相交就返回交点, 不相交最后就是null==null
+        while(pA != pB) {
+            pA = pA == NULL ? headB : pA->next;
+            pB = pB == NULL ? headA : pB->next;
+        }
+        return pA;
+    }
+};
